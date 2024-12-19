3f9b9199bcaa1d82f2d09de57d1ffe2457813406
Paul Bennett <pbennett.uk@gmail.com>
2024-12-19 16:00:42 +0000

    trying

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,15 +3,15 @@ import "./globals.css";
 
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import ThemeSwitcher from "@/components/themeSwitcher";
+// import ThemeSwitcher from "@/components/themeSwitcher";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="container mx-auto flex min-h-screen flex-col p-4 font-serif md:max-w-[75%] md:p-0">
-        <div className="fixed right-4 top-4 z-50 hidden md:block">
+        {/* <div className="fixed right-4 top-4 z-50 hidden md:block">
           <ThemeSwitcher />
-        </div>
+        </div> */}
         <Header />
         <div className="flex-auto">{children}</div>
         <Footer />
