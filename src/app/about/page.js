73022ce789bb8ff6ed55cc3c14df909b7486f0a4
Paul Bennett <pbennett.uk@gmail.com>
2025-01-07 19:04:52 +0000

    fixing formating

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -77,33 +77,33 @@ export default function About() {
           <h2>Why this blog?</h2>
 
           <p>
-            Welcome to my corner of the digital universe! II&apos;m a remote
-            Lead Solutions Engineer with a passion for solving complex problems,
+            Welcome to my corner of the digital universe! I&apos;m a remote Lead
+            Solutions Engineer with a passion for solving complex problems,
             especially in the realms of databases, SQL, and automation. While my
             Kubernetes cluster keeps me busy, my true excitement lies in
             crafting database solutions and building scalable systems that push
-            boundaries—all within a tech playground thatI&apos;s both
+            boundaries—all within a tech playground that&apos;s both
             cutting-edge and wife-approved.
           </p>
 
           <p>
             This site is a reflection of my journey as I grow into a future CTO.
-            II&apos;m fascinated by the possibilities of AI, large language
+            I&apos;m fascinated by the possibilities of AI, large language
             models (LLMs), image generation, and the vast opportunities in Big
             Data. Alongside these technical interests, I find joy in leadership,
             management, and exploring productivity strategies that empower both
             teams and individuals.
           </p>
           <p>
-            What youI&apos;ll find here is a mix of code snippets, tech
+            What you&apos;ll find here is a mix of code snippets, tech
             explorations, lessons learned, and candid insights into database
-            engineering and administration. II&apos;ve traded the obsession over
+            engineering and administration. I&apos;ve traded the obsession over
             aesthetics for a focus on content—sharing ideas, innovations, and
             the occasional caffeine-inspired epiphany.
           </p>
 
           <p>
-            Thanks for stopping by! HereI&apos;s to navigating the ever-evolving
+            Thanks for stopping by! Here&apos;s to navigating the ever-evolving
             tech landscape together.
           </p>
         </article>
